9affa2fefabad1c7275d1bb3fa823b5c8b177384
MNathan777 <miguelnathan606@gmail.com>
2025-08-25 11:01:27 -0300

    Menu

---
+++ src/Menu.jsx
@@ -0,0 +1,30 @@
+const Menu = () => {
+
+    return (
+        <div>
+            <ul className="nav">
+                <li className="nav-item">
+                    <a class="nav-link" href="#">
+                        Active
+
+                    </a>
+                </li>
+            </ul>
+             <ul className="nav">
+                <li className="nav-item">
+                    <a class="nav-link" href="home">
+                        Home
+                        
+                    </a>
+                </li>
+            </ul> <ul className="nav">
+                <li className="nav-item">
+                    <a class="nav-link" href="viacep">
+                        ViaCep
+                        
+                    </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
